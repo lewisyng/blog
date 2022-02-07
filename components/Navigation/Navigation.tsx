dc88c6ede4098ed8dc2a787d50e1219dafe7e3ae
lewisyng <lewisyoung1306@yahoo.de>
2022-02-07 20:47:07 +0100

    clean up linting

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,13 +1,16 @@
 import styles from './Navigation.module.css'
 import BookmarksOutlinedIcon from '@mui/icons-material/BookmarksOutlined'
 import Logo from '../UI/Logo/Logo'
+import Link from 'next/link'
 
 export const Navigation = () => {
   return (
     <div className={styles.navigation}>
-      <a href="/" className={styles.navigation__logo}>
-        <Logo />
-      </a>
+      <Link href="/" passHref>
+        <div className={styles.navigation__logo}>
+          <Logo />
+        </div>
+      </Link>
 
       {/* <div className={styles.navigation__actions}>
         <BookmarksOutlinedIcon />
