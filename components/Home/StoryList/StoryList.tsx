f317543825d48315b389c6a4fe4cf11008d9dd4a
lewisyng <lewisyoung1306@yahoo.de>
2022-02-20 00:45:12 +0100

    checking if any posts where fetched

--- components/Home/StoryList/StoryList.tsx
+++ components/Home/StoryList/StoryList.tsx
@@ -4,9 +4,8 @@ import StoryListItem from '../StoryListItem/StoryListItem'
 export const StoryList = ({ posts }: { posts: [Post] }) => {
   return (
     <div className="storyList mt-[2rem]">
-      {posts.map((post: Post) => (
-        <StoryListItem key={post._id} post={post} />
-      ))}
+      {posts &&
+        posts.map((post: Post) => <StoryListItem key={post._id} post={post} />)}
     </div>
   )
 }
